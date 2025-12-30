74ca12fba6c2b6ec1acc97b758394b7841c8d3be
Mark Goodwin <m0good04@gmail.com>
2025-12-30 18:26:59 -0500

    Added the font files and some basic scss styling.

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,36 @@
-<style scoped>
+
+<script setup lang="ts">
+import { onMounted } from "vue";
+import { useTracksStore } from "./stores/tracks";
+import "./scss/_main.scss";
+
+const tracksStore = useTracksStore();
+
+onMounted(() => {
+  tracksStore.loadTracks();
+});
+</script>
+
+<template>
+  <main class="container">
+    <section>
+      <h2>Tracks</h2>
+      <div v-if="tracksStore.loading">Loading...</div>
+      <div v-if="tracksStore.error" style="color: red">{{ tracksStore.error }}</div>
+      <textarea
+        rows="10"
+        style="width: 100%; margin-top: 1em;"
+        readonly
+        :value="Array.from(tracksStore.tracks.entries())
+          .sort(([a], [b]) => a.localeCompare(b))
+          .map(([key, value]) => `${key}: ${value}`)
+          .join('\n')"
+      ></textarea>
+    </section>
+  </main>
+</template>
+
+<style lang="scss" scoped>
 .container {
   margin: 0 auto;
   padding: 2em;
@@ -43,33 +75,4 @@ h2 {
   margin-bottom: 1em;
 }
 </style>
-<script setup lang="ts">
-import { onMounted } from "vue";
-import { useTracksStore } from "./stores/tracks";
-
-const tracksStore = useTracksStore();
-
-onMounted(() => {
-  tracksStore.loadTracks();
-});
-</script>
-
-<template>
-  <main class="container">
-    <section>
-      <h2>Tracks</h2>
-      <div v-if="tracksStore.loading">Loading...</div>
-      <div v-if="tracksStore.error" style="color: red">{{ tracksStore.error }}</div>
-      <textarea
-        rows="10"
-        style="width: 100%; margin-top: 1em;"
-        readonly
-        :value="Array.from(tracksStore.tracks.entries())
-          .sort(([a], [b]) => a.localeCompare(b))
-          .map(([key, value]) => `${key}: ${value}`)
-          .join('\n')"
-      ></textarea>
-    </section>
-  </main>
-</template>
 
