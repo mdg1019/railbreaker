4f5e4038f1c87a1f3130d553e2f8ca693d65f274
Mark Goodwin <m0good04@gmail.com>
2026-01-17 13:36:09 -0500

    Started adding print colors.

--- src/views/PrintView.vue
+++ src/views/PrintView.vue
@@ -4,6 +4,7 @@ import { listen, type UnlistenFn } from "@tauri-apps/api/event";
 import { getCurrentWindow } from "@tauri-apps/api/window";
 import type { RaceCardPrintPayload } from "../models/print";
 import { PRINT_PAYLOAD_EVENT } from "../utils/openPrintWindowEvent";
+import "../scss/_main.scss";
 
 const payload = ref<RaceCardPrintPayload | null>(null);
 let unlisten: UnlistenFn | null = null;
@@ -46,6 +47,7 @@ async function doPrintAndClose() {
 }
 
 onMounted(async () => {
+    document.body.classList.add("print-preview");
     unlisten = await listen<RaceCardPrintPayload>(PRINT_PAYLOAD_EVENT, async (event) => {
         payload.value = event.payload;
         await doPrintAndClose();
@@ -59,25 +61,37 @@ onMounted(async () => {
 });
 
 onBeforeUnmount(() => {
+    document.body.classList.remove("print-preview");
     if (unlisten) unlisten();
     unlisten = null;
 });
 </script>
 
 <template>
-    <div class="page">
-        <header class="hdr">
-            <h1 class="title">{{ payload?.raceCard.track ?? "Print" }}</h1>
-         </header>
+    <div class="container">
+        <div class="page">
+            <header class="hdr">
+                <h1 class="title">{{ payload?.raceCard.track ?? "Print" }}</h1>
+            </header>
 
-        <main v-if="payload">  
-            Race Card Goes Here
-        </main>
+            <main v-if="payload">
+                Race Card Goes Here
+            </main>
+        </div>
     </div>
 </template>
 
+<style lang="scss" scoped>
+.container {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: var(--bg);
+    color: var(--fg);
+}
 
-<style scoped>
 .page {
     font-family: MGSans, sans-serif;
     padding: 18px;
