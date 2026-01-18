13adbd46ac054da61d31a3f8d810cd5d395a17f1
Mark Goodwin <m0good04@gmail.com>
2026-01-18 06:47:17 -0500

    Tightened up the code.

--- src/components/racecard/RacecardHeader.vue
+++ src/components/racecard/RacecardHeader.vue
@@ -9,41 +9,25 @@ const props = withDefaults(defineProps<{
     racecard: Racecard;
     race: number;
     print?: boolean;
-    headerFontSize?: string;
-    trackFontSize?: string;
-    raceNumberFontSize?: string;
 }>(), {
     print: false,
 });
 
-
-const headerFontSize = computed(() => props.headerFontSize ?? (props.print ? "1rem" : "1.6rem"));
-const trackFontSize = computed(() => props.trackFontSize ?? (props.print ? "1.2rem" : "2rem"));
-const raceNumberFontSize = computed(() => props.raceNumberFontSize ?? (props.print ? "1.2rem" : "2rem"));
 const raceIndex = computed(() => Math.max(0, props.race - 1));
 const currentRace = computed(() => props.racecard.races[raceIndex.value]);
 </script>
 
 <template>
-    <Panel v-if="!props.print">
-        <div class="race-container-header" :style="{ '--headerFontSize': headerFontSize }">
-            <div class="track-name color-accent-green" :style="{ '--trackFontSize': trackFontSize }">{{ props.racecard.track }}</div>
+    <Panel :print="props.print">
+        <div class="race-container-header" :style="{ '--headerFontSize': props.print ? '1rem' : '1.6rem' }">
+            <div class="track-name color-accent-green" :style="{ '--trackFontSize': props.print ? '1.2rem' : '2rem' }">{{ props.racecard.track }}</div>
             <RaceClassification class="raceType color-accent-yellow" :race="currentRace" :prefix-color="'var(--accent-green)'" />
             <div class=" color-accent-yellow">{{ Transformers.getDistanceLength(currentRace?.distance ?? 0) }}</div>
             <div class=" color-accent-yellow">{{ Transformers.getAgeSexRestrictionString(currentRace?.ageSexRestrictions) }}</div>
             <div class="race-date color-accent-yellow">{{ props.racecard.longDate }}</div>
-            <div class="race-number color-accent-green" :style="{ '--raceNumberFontSize': raceNumberFontSize }">Race {{ props.race }}</div>
+            <div class="race-number color-accent-green" :style="{ '--raceNumberFontSize': props.print ? '1.2rem' : '2rem' }">Race {{ props.race }}</div>
         </div>
     </Panel>
-
-    <div v-else class="race-container-header" :style="{ '--headerFontSize': headerFontSize }">
-        <div class="track-name color-accent-green" :style="{ '--trackFontSize': trackFontSize }">{{ props.racecard.track }}</div>
-        <RaceClassification class="raceType color-accent-yellow" :race="currentRace" :prefix-color="'var(--accent-green)'" />
-        <div class=" color-accent-yellow">{{ Transformers.getDistanceLength(currentRace?.distance ?? 0) }}</div>
-        <div class=" color-accent-yellow">{{ Transformers.getAgeSexRestrictionString(currentRace?.ageSexRestrictions) }}</div>
-        <div class="race-date color-accent-yellow">{{ props.racecard.longDate }}</div>
-        <div class="race-number color-accent-green" :style="{ '--raceNumberFontSize': raceNumberFontSize }">Race {{ props.race }}</div>
-    </div>
 </template>
 
 <style scoped lang="scss">
--- src/components/ui/Panel.vue
+++ src/components/ui/Panel.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
+    const props = withDefaults(defineProps<{
+        print?: boolean;
+    }>(), {
+        print: false,
+    });
 </script>
 
 <template>
-    <div class="panel">
+    <div class="panel" :class="{ 'print': props.print }">
         <slot />
     </div>
 </template>
@@ -16,5 +21,14 @@
     border: 2px solid var(--ubuntu-blue);
     border-radius: 6px;
     background: var(--background-light);
+}
+
+.panel.print {
+    box-sizing: border-box;
+    margin-top: .5rem;
+    margin-bottom: .5rem;
+    border: none;
+    border-radius: 0;
+    background: transparent;
 }
 </style>
